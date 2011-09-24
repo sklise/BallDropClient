86882905e1770ba15d02e95d8a40d137a4eaba50
Steven Klise <steven@stevenklise.com>
2011-09-24 15:51:43 -0400

    Consolidated controllers into generic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,44 +3,9 @@ get "/" do
   erb :main
 end
 
-post "/info" do
-  t = Task.new(:command => "i", :done => false)
+post "/:command" do
+  t = Task.new(:command => params[:command], :done => false)
   t.save
-  redirect "/"
-end
-
-post "/connect" do
-  t = Task.new(:command => "connect", :done => false)
-  t.save
-  redirect "/"
-end
-
-post "/right" do
-  t = Task.new(:command => "r", :done => false)
-  t.save
-  redirect "/"
-end
-
-post "/left" do
-  t = Task.new(:command => "l", :done => false)
-  t.save
-  redirect "/"
-end
-
-post "/up" do
-  t = Task.new(:command => "u", :done => false)
-  t.save
-  redirect "/"
-end
-
-post "/down" do
-  t = Task.new(:command => "d", :done => false)
-  t.save  
-  redirect "/"
-end
-
-post "/disconnect" do
-  t = Task.new(:command => "x", :done => false)
-  t.save
+  "command sent"
   redirect "/"
 end
