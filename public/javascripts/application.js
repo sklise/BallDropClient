4dc75085bf31306f72cf24c0089ca61fd3424b63
Steven Klise <steven@stevenklise.com>
2011-09-24 17:14:19 -0400

    implemented ajax requests with query

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
-  
+  $('#connect').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'connect'
+    })
+  });
+  $('#disconnect').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'x'
+    })
+  });
+  $('#info').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'i'
+    })
+  });
+  $('#arrow-up').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'u'
+    })
+  });
+  $('#arrow-down').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'd'
+    })
+  });
+  $('#arrow-right').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'r'
+    })
+  });
+  $('#arrow-left').click(function(){
+    $.ajax({
+      type: 'POST',
+      url: 'l'
+    })
+  });
 });
