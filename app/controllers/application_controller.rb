9e1c02664ca8b8a8dc9f96d943650e5deacdcd6b
Steven Klise <steven@stevenklise.com>
2011-09-23 20:13:14 -0400

    Controller for each command

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,38 @@ get "/" do
   erb :main
 end
 
-get "/connect" do
-  erb :main
+post "/connect" do
+  t = Task.new(:command => "connect", :done => false)
+  t.save
+  redirect "/"
 end
 
 post "/right" do
-  
+  t = Task.new(:command => "r", :done => false)
+  t.save
+  redirect "/"
 end
 
 post "/left" do
-  
+  t = Task.new(:command => "l", :done => false)
+  t.save
+  redirect "/"
 end
 
 post "/up" do
-  
+  t = Task.new(:command => "u", :done => false)
+  t.save
+  redirect "/"
 end
 
 post "/down" do
-  
+  t = Task.new(:command => "d", :done => false)
+  t.save  
+  redirect "/"
 end
 
 post "/disconnect" do
-  
+  t = Task.new(:command => "x", :done => false)
+  t.save
+  redirect "/"
 end
