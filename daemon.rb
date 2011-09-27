0aa2c290874203f4870d0faf358978cd674117ef
Steven Klise <steven@stevenklise.com>
2011-09-27 11:13:44 -0400

    Removed autoconnection.

--- daemon.rb
+++ daemon.rb
@@ -1,9 +1,7 @@
 require 'net/telnet'
 require 'yaml'
-@config = YAML::load(File.open('database.yml'))
+@config = YAML::load(File.open('./database.yml'))
 require './app/models/Task.rb'
-
-@host = Net::Telnet.new("Host" => "128.122.151.161", "Port" => 8080, "Timeout"=>25, "Output_log"=>"output_log.log", "Dump_log"=> "dump_log.log", "Prompt"=> /C:.*>/)
 
 while true do
 
